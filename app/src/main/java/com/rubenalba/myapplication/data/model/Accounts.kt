372a4e1dfb0cce0cc90ce7494dee09a81adc7dd1
Rubén <ru.alba.dev@gmail.com>
2026-01-16 20:00:55 +0100

    Data layer (#1)
    * prepare dependencies
    
    * create folder entity
    
    * create accounts entity
    
    * create user entity

---
+++ app/src/main/java/com/rubenalba/myapplication/data/model/Accounts.kt
@@ -0,0 +1,61 @@
+package com.rubenalba.myapplication.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "accounts",
+    foreignKeys = [
+        ForeignKey(
+            entity = Folder::class,
+            parentColumns = ["id"],
+            childColumns = ["folder_id"],
+            onDelete = ForeignKey.SET_NULL // if the folder is deleted, folder id = null
+        )
+    ]
+)
+data class Accounts(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    @ColumnInfo(name = "folder_id", index = true)
+    val folderId: Long? = null, // can be null (account without folder)
+
+    @ColumnInfo(name = "service_name")
+    val serviceName: String,
+
+    @ColumnInfo(name = "encrypted_email")
+    val encryptedEmail: String? = null,
+
+    @ColumnInfo(name = "iv_email")
+    val ivEmail: String? = null,
+
+    @ColumnInfo(name = "encrypted_username")
+    val encryptedUsername: String? = null,
+
+    @ColumnInfo(name = "iv_username")
+    val ivUsername: String? = null,
+
+    @ColumnInfo(name = "encrypted_password")
+    val encryptedPassword: String,
+
+    @ColumnInfo(name = "iv_password")
+    val ivPassword: String,
+
+    @ColumnInfo(name = "encrypted_url")
+    val encryptedUrl: String? = null,
+
+    @ColumnInfo(name = "iv_url")
+    val ivUrl: String? = null,
+
+    @ColumnInfo(name = "encrypted_notes")
+    val encryptedNotes: String? = null,
+
+    @ColumnInfo(name = "iv_notes")
+    val ivNotes: String? = null,
+
+    @ColumnInfo(name = "last_modified")
+    val lastModified: Long = System.currentTimeMillis()
+)
