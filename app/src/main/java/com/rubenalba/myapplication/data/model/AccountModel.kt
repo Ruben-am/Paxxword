6c68a014ecd0495f1cf861fb3ab58d7ea7f76bfb
Ruben-am <ru.alba.dev@gmail.com>
2026-01-17 13:37:16 +0100

    fix account model

--- app/src/main/java/com/rubenalba/myapplication/data/model/AccountModel.kt
+++ app/src/main/java/com/rubenalba/myapplication/data/model/AccountModel.kt
@@ -4,8 +4,10 @@ data class AccountModel(
     val id: Long = 0,
     val folderId: Long? = null,
     val serviceName: String,
-    val username: String,
-    val password: String,
+
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
     val url: String = "",
     val notes: String = ""
 )
