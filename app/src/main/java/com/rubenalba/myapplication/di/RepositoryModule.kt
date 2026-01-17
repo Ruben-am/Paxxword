4096ded7afad596315ce38d75e6f926582f44adb
Ruben-am <ru.alba.dev@gmail.com>
2026-01-17 14:54:10 +0100

    create repository module

---
+++ app/src/main/java/com/rubenalba/myapplication/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.rubenalba.myapplication.di
+
+import com.rubenalba.myapplication.data.repository.PasswordRepository
+import com.rubenalba.myapplication.data.repository.PasswordRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindPasswordRepository(
+        impl: PasswordRepositoryImpl
+    ): PasswordRepository
+}
