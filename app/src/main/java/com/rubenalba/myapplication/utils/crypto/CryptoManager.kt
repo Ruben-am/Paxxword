6a0ba0a5998ba8b11d870363e2de176fd90ad28e
Ruben-am <ru.alba.dev@gmail.com>
2026-01-22 18:50:25 +0100

    set gcm algorithm

--- app/src/main/java/com/rubenalba/myapplication/utils/crypto/CryptoManager.kt
+++ app/src/main/java/com/rubenalba/myapplication/utils/crypto/CryptoManager.kt
@@ -3,38 +3,47 @@ package com.rubenalba.myapplication.utils.crypto
 import android.util.Base64
 import java.security.SecureRandom
 import javax.crypto.Cipher
+import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 object CryptoManager {
 
-    private const val ALGORITHM = "AES/CBC/PKCS5Padding"
+    private const val ALGORITHM = "AES/GCM/NoPadding"
+
+    private const val IV_LENGTH = 12
+
+    private const val TAG_LENGTH_BITS = 128
 
     // create a new random IV for each piece of data stored
     fun generateIv(): ByteArray {
-        val iv = ByteArray(16)
+        val iv = ByteArray(IV_LENGTH)
         SecureRandom().nextBytes(iv)
         return iv
     }
 
     // encrypt: Text -> Bytes -> Encryption -> Base64
-    fun encrypt(plaintext: String, secretKey: SecretKeySpec, iv: ByteArray): String {
+    fun encrypt(plaintext: ByteArray, secretKey: SecretKeySpec, iv: ByteArray): String {
         val cipher = Cipher.getInstance(ALGORITHM)
-        val ivSpec = IvParameterSpec(iv)
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec)
 
-        val encryptedBytes = cipher.doFinal(plaintext.toByteArray(Charsets.UTF_8))
+        val spec = GCMParameterSpec(TAG_LENGTH_BITS, iv)
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, spec)
+
+        val encryptedBytes = cipher.doFinal(plaintext)
+
         return Base64.encodeToString(encryptedBytes, Base64.NO_WRAP)
     }
 
     // Decrypt: Base64 -> Bytes -> Decryption -> Text
     fun decrypt(ciphertextBase64: String, secretKey: SecretKeySpec, iv: ByteArray): String {
         val cipher = Cipher.getInstance(ALGORITHM)
-        val ivSpec = IvParameterSpec(iv)
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec)
+        val spec = GCMParameterSpec(TAG_LENGTH_BITS, iv)
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, spec)
 
         val decodedBytes = Base64.decode(ciphertextBase64, Base64.NO_WRAP)
+
         val decryptedBytes = cipher.doFinal(decodedBytes)
+
         return String(decryptedBytes, Charsets.UTF_8)
     }
 
