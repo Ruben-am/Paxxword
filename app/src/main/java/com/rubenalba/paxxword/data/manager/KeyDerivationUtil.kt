aa81f90f78a058703cd8565fd39a9a6adbeb2e7c
Ruben-am <ru.alba.dev@gmail.com>
2026-01-30 22:53:42 +0100

    change iterations

--- app/src/main/java/com/rubenalba/paxxword/data/manager/KeyDerivationUtil.kt
+++ app/src/main/java/com/rubenalba/paxxword/data/manager/KeyDerivationUtil.kt
@@ -7,7 +7,7 @@ import javax.crypto.spec.SecretKeySpec
 
 object KeyDerivationUtil {
 
-    private const val ITERATIONS = 600000 // Repeat the process 10k times to make it "slow" and safe
+    private const val ITERATIONS = 300000 // Repeat the process 300k times to make it "slow" and safe
     private const val KEY_LENGTH = 256   // Standard military security size (AES-256)
     private const val ALGORITHM = "PBKDF2WithHmacSHA256"
 
