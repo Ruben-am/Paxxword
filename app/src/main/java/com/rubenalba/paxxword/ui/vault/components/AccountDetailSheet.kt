5ca94156381772d9f941a835b6e25693da362c0e
Rubén <ru.alba.dev@gmail.com>
2026-01-31 11:12:04 +0100

    Optimization (#37)
    * fix splash to auth problem
    
    * fix paxxword icons and adjust splash delay
    
    * change iterations
    
    * fix keyboard behavior
    
    * set app icon

--- app/src/main/java/com/rubenalba/paxxword/ui/vault/components/AccountDetailSheet.kt
+++ app/src/main/java/com/rubenalba/paxxword/ui/vault/components/AccountDetailSheet.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -110,6 +111,7 @@ fun AccountDetailContent(
             .padding(horizontal = 24.dp)
             .padding(bottom = 48.dp)
             .verticalScroll(rememberScrollState())
+            .imePadding()
     ) {
         // header
         Row(
