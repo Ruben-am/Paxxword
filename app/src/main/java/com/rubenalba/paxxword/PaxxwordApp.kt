b623d682bd684564a892a99954872b5e0e64366a
Ruben-am <ru.alba.dev@gmail.com>
2026-06-19 16:30:17 +0200

    clean paxxwordapp

--- app/src/main/java/com/rubenalba/paxxword/PaxxwordApp.kt
+++ app/src/main/java/com/rubenalba/paxxword/PaxxwordApp.kt
@@ -1,9 +1,6 @@
 package com.rubenalba.paxxword
 
 import android.app.Application
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.ProcessLifecycleOwner
 import com.rubenalba.paxxword.data.manager.SessionManager
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
@@ -16,14 +13,5 @@ class PaxxwordApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        val appLifecycleObserver = object : DefaultLifecycleObserver {
-            override fun onStop(owner: LifecycleOwner) {
-                super.onStop(owner)
-                sessionManager.clearSession()
-            }
-        }
-
-        ProcessLifecycleOwner.get().lifecycle.addObserver(appLifecycleObserver)
     }
 }
