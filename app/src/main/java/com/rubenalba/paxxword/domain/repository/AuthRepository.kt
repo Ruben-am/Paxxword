2b2509912ab2fd6c619dbf8a2de695b620390292
Ruben-am <ru.alba.dev@gmail.com>
2026-06-19 18:36:37 +0200

    modify domain layer

--- app/src/main/java/com/rubenalba/paxxword/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/rubenalba/paxxword/domain/repository/AuthRepository.kt
@@ -1,7 +1,7 @@
 package com.rubenalba.paxxword.domain.repository
 
 interface AuthRepository {
-    suspend fun changeMasterPassword(newPassword: String): Boolean
+    suspend fun changeMasterPassword(newPassword: CharArray): Boolean
     suspend fun login(password: CharArray): Boolean
     suspend fun register(password: CharArray, defaultEmailLabel: String)
     suspend fun restoreFromBackup(uriString: String, password: CharArray, defaultEmailLabel: String): Boolean
