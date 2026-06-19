174bfe7150b44bc0b4793db7f3afc5fbd077e632
Ruben-am <ru.alba.dev@gmail.com>
2026-06-19 11:26:40 +0200

    update dependecies inyections

--- app/src/main/java/com/rubenalba/paxxword/di/RepositoryModule.kt
+++ app/src/main/java/com/rubenalba/paxxword/di/RepositoryModule.kt
@@ -1,7 +1,11 @@
 package com.rubenalba.paxxword.di
 
-import com.rubenalba.paxxword.domain.repository.PasswordRepository
-import com.rubenalba.paxxword.data.repository.PasswordRepositoryImpl
+import com.rubenalba.paxxword.data.repository.AccountRepositoryImpl
+import com.rubenalba.paxxword.data.repository.AuthRepositoryImpl
+import com.rubenalba.paxxword.data.repository.FolderRepositoryImpl
+import com.rubenalba.paxxword.domain.repository.AccountRepository
+import com.rubenalba.paxxword.domain.repository.AuthRepository
+import com.rubenalba.paxxword.domain.repository.FolderRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,7 +18,19 @@ abstract class RepositoryModule {
 
     @Binds
     @Singleton
-    abstract fun bindPasswordRepository(
-        impl: PasswordRepositoryImpl
-    ): PasswordRepository
+    abstract fun bindAccountRepository(
+        impl: AccountRepositoryImpl
+    ): AccountRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindFolderRepository(
+        impl: FolderRepositoryImpl
+    ): FolderRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(
+        impl: AuthRepositoryImpl
+    ): AuthRepository
 }
