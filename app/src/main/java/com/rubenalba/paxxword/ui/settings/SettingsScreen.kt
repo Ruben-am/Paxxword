81990282e01726f24e18c65e432dc0c774d1c103
Ruben-am <ru.alba.dev@gmail.com>
2026-06-25 14:12:26 +0200

    add icon to dinamic color switch

--- app/src/main/java/com/rubenalba/paxxword/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/rubenalba/paxxword/ui/settings/SettingsScreen.kt
@@ -41,6 +41,9 @@ import com.rubenalba.paxxword.domain.model.AppTheme
 import com.rubenalba.paxxword.ui.theme.JetBrainsMonoFontFamily
 import com.rubenalba.paxxword.util.Constants
 import kotlinx.coroutines.launch
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.foundation.layout.size
 
 enum class ChangePasswordStep { NONE, VERIFY_CURRENT, ENTER_NEW }
 
@@ -234,7 +237,18 @@ fun SettingsScreen(
                         action = {
                             Switch(
                                 checked = state.useDynamicColor,
-                                onCheckedChange = viewModel::updateDynamicColor
+                                onCheckedChange = viewModel::updateDynamicColor,
+                                thumbContent = if (state.useDynamicColor) {
+                                    {
+                                        Icon(
+                                            imageVector = Icons.Default.Check,
+                                            contentDescription = null,
+                                            modifier = Modifier.size(SwitchDefaults.IconSize),
+                                        )
+                                    }
+                                } else {
+                                    null
+                                }
                             )
                         }
                     )
